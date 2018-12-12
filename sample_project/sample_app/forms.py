04c51af196ce7d5dd22b829422893a441de8c262
Santiago Basulto <santiago.basulto@gmail.com>
2018-12-12 00:16:44 -0300

    Forms

--- sample_project/sample_app/forms.py
+++ sample_project/sample_app/forms.py
@@ -8,3 +8,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'isbn', 'popularity']
+
+# Old one:
+# class BookForm(forms.Form):
+#     title = forms.CharField(label='Title', max_length=200)
+#     author = forms.ModelChoiceField(label='Author', queryset=Author.objects.all())
+#     isbn = forms.CharField(label='ISBN', max_length=20)
+#     popularity = forms.FloatField(label='Popularity', min_value=0)
